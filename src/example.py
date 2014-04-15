6e6a9516ad83c02b705ad981bd3fd4a0f37975cc
Andrew Ning <andrew.ning@nrel.gov>
2014-04-15 16:08:15 -0600

    cleanup and using new io format

--- src/example.py
+++ src/example.py
@@ -1,48 +1,58 @@
 #!/usr/bin/env python
 # encoding: utf-8
 """
-harpopt_example.py
+example.py
 
 Created by Andrew Ning on 2014-03-18.
 Copyright (c) NREL. All rights reserved.
 """
 
 import numpy as np
+import os
 
 from harpopt import HARPOptCCBlade
-from rotorse.rotoraerodefaults import CSMDrivetrain, RayleighCDF, WeibullCDF
 
-
-# use_snopt = False
+# ----- options ----------
+varspeed = True
+varpitch = True
+cdf_type = 'rayleigh'
 use_snopt = True
-rotor = HARPOptCCBlade(use_snopt)
+# ------------------------
 
-# -------- start inputs -------------
+# --- instantiate rotor object -------
+rotor = HARPOptCCBlade(varspeed, varpitch, cdf_type, use_snopt)
+# ------------------------------------
 
-# --- rotor geometry ---
-rotor.r_max_chord = 0.23577
-rotor.chord_sub = [3.2612, 4.5709, 3.3178, 1.4621]
-rotor.theta_sub = [13.2783, 7.46036, 2.89317, -0.0878099]
-rotor.Rhub = 1.5
-rotor.Rtip = 63.0
-rotor.precone = 2.5
-rotor.tilt = -5.0
-rotor.yaw = 0.0
-rotor.B = 3
 
-# --- airfoils ---
-basepath = '/Users/Andrew/Dropbox/NREL/5MW_files/5MW_AFFiles/'
+# **dv** indicates that this variable is a design variable for the optimization
+# (under the default setup) and the value you supply will be used as a starting point
+
+
+# ------- rotor geometry ------------
+rotor.r_max_chord = 0.23577  # **dv** (Float): location of second control point (generally also max chord)
+rotor.chord_sub = [3.2612, 4.5709, 3.3178, 1.4621]  # **dv** (Array, m): chord at control points
+rotor.theta_sub = [13.2783, 7.46036, 2.89317, -0.0878099]  # **dv** (Array, deg): twist at control points
+rotor.Rhub = 1.5  # (Float, m): hub radius
+rotor.Rtip = 63.0  # (Float, m): tip radius
+rotor.precone = 2.5  # (Float, deg): precone angle
+rotor.tilt = -5.0  # (Float, deg): shaft tilt
+rotor.yaw = 0.0  # (Float, deg): yaw error
+rotor.B = 3  # (Int): number of blades
+# -------------------------------------
+
+# ------------- airfoils ------------
+basepath = '5MW_AFFiles'
 
 # load all airfoils
 airfoil_types = [0]*8
-airfoil_types[0] = basepath + 'Cylinder1.dat'
-airfoil_types[1] = basepath + 'Cylinder2.dat'
-airfoil_types[2] = basepath + 'DU40_A17.dat'
-airfoil_types[3] = basepath + 'DU35_A17.dat'
-airfoil_types[4] = basepath + 'DU30_A17.dat'
-airfoil_types[5] = basepath + 'DU25_A17.dat'
-airfoil_types[6] = basepath + 'DU21_A17.dat'
-airfoil_types[7] = basepath + 'NACA64_A17.dat'
+airfoil_types[0] = basepath + os.path.sep + 'Cylinder1.dat'
+airfoil_types[1] = basepath + os.path.sep + 'Cylinder2.dat'
+airfoil_types[2] = basepath + os.path.sep + 'DU40_A17.dat'
+airfoil_types[3] = basepath + os.path.sep + 'DU35_A17.dat'
+airfoil_types[4] = basepath + os.path.sep + 'DU30_A17.dat'
+airfoil_types[5] = basepath + os.path.sep + 'DU25_A17.dat'
+airfoil_types[6] = basepath + os.path.sep + 'DU21_A17.dat'
+airfoil_types[7] = basepath + os.path.sep + 'NACA64_A17.dat'
 
 # place at appropriate radial stations
 af_idx = [0, 0, 1, 2, 3, 3, 4, 5, 5, 6, 6, 7, 7, 7, 7, 7, 7]
@@ -52,62 +62,61 @@ af = [0]*n
 for i in range(n):
     af[i] = airfoil_types[af_idx[i]]
 
-rotor.airfoil_files = af
-rotor.r_af = np.array([0.02222276, 0.06666667, 0.11111057, 0.16666667, 0.23333333, 0.3, 0.36666667, 0.43333333, 0.5, 0.56666667, 0.63333333, 0.7, 0.76666667, 0.83333333, 0.88888943, 0.93333333, 0.97777724])
-rotor.idx_cylinder = 3
+rotor.airfoil_files = af  # (List): paths to AeroDyn-style airfoil files
+rotor.r_af = np.array([0.02222276, 0.06666667, 0.11111057, 0.16666667, 0.23333333, 0.3, 0.36666667,
+    0.43333333, 0.5, 0.56666667, 0.63333333, 0.7, 0.76666667, 0.83333333, 0.88888943,
+    0.93333333, 0.97777724])    # (Array, m): locations where airfoils are defined on unit radius
+rotor.idx_cylinder = 3  # (Int): index in r_af where cylinder section ends
+# -------------------------------------
 
-# --- site characteristics ---
-rotor.rho = 1.225
-rotor.mu = 1.81206e-5
-rotor.shearExp = 0.2
-rotor.hubHt = 80.0
-
-cdf_type = 'rayleigh'
-
-if cdf_type == 'rayleigh':
-    rotor.replace('cdf', RayleighCDF())
-    rotor.cdf.xbar = 6.0
-
-elif cdf_type == 'weibull':
-    rotor.replace('cdf', WeibullCDF())
-    rotor.cdf.A = 6.0
-    rotor.cdf.k = 2.0
+# ------- site characteristics --------
+rotor.rho = 1.225  # (Float, kg/m**3): density of air
+rotor.mu = 1.81206e-5  # (Float, kg/m/s): dynamic viscosity of air
+rotor.shearExp = 0.2  # (Float): shear exponent
+rotor.hubHt = 80.0  # (Float, m)
+rotor.cdf_mean_wind_speed = 6.0  # (Float, m/s): mean wind speed of site cumulative distribution function
+if cdf_type == 'weibull':
+    rotor.weibull_shape_factor = 2.0  # (Float): shape factor of weibull distribution
+# -------------------------------------
 
 
-# --- control settings ---
-rotor.control.Vin = 3.0
-rotor.control.Vout = 25.0
-rotor.control.ratedPower = 5e6
-rotor.control.minOmega = 0.0
-rotor.control.maxOmega = 12.0
-rotor.control.pitch = 0.0
-rotor.control.tsr = 7.55
+# ------- control settings ------------
+rotor.control.Vin = 3.0  # (Float, m/s): cut-in wind speed
+rotor.control.Vout = 25.0  # (Float, m/s): cut-out wind speed
+rotor.control.ratedPower = 5e6  # (Float, W): rated power
+rotor.control.pitch = 0.0  # (Float, deg): pitch angle in region 2 (and region 3 for fixed pitch machines)
+
+if varspeed:
+    rotor.control.minOmega = 0.0  # (Float, rpm): minimum allowed rotor rotation speed
+    rotor.control.maxOmega = 12.0  # (Float, rpm): maximum allowed rotor rotation speed
+    rotor.control.tsr = 7.55  # **dv** (Float): tip-speed ratio in Region 2 (should be optimized externally)
+else:
+    rotor.control.Omega = 8.0    # (Float, rpm): fixed rotor rotation speed
+# -------------------------------------
+
+# ------ drivetrain model for efficiency --------
+rotor.drivetrainType = 'geared'
+# -------------------------------------
 
 
-# --- drivetrain efficiency ---
-rotor.replace('dt', CSMDrivetrain())
-rotor.dt.missing_deriv_policy = 'assume_zero'  # TODO: openmdao bug remove later
-rotor.dt.drivetrainType = 'geared'
+# ------- analysis options ------------
+rotor.nSector = 4  # (Int): number of sectors to divide rotor face into in computing thrust and power
+rotor.npts_coarse_power_curve = 20  # (Int): number of points to evaluate aero analysis at
+rotor.npts_spline_power_curve = 200  # (Int): number of points to use in fitting spline to power curve
+rotor.AEP_loss_factor = 1.0  # (Float): availability and other losses (soiling, array, etc.)
+# -------------------------------------
 
 
-# --- options ---
-rotor.nSector = 4
-rotor.npts_coarse_power_curve = 20
-rotor.npts_spline_power_curve = 200
-rotor.AEP_loss_factor = 1.0
-# optimize_stations = True
-
-# -------- end of inputs -------------
+############## end of inputs ###################
 
 
-# # run once to get baseline for normalization
-# rotor.driver.run_iteration()
-# rotor.AEP0 = rotor.aep.AEP
-# rotor.driver.run_iteration()
+# run one iteration to get baseline for normalization
+rotor.driver.run_iteration()
+rotor.AEP0 = rotor.aep.AEP
+rotor.driver.run_iteration()
 
-rotor.AEP0 = 9716743.98384
-
-
+# run optimization
 rotor.run()
+
 
 
